ee67d6c403efe2d2550d9a49c7c829c1f514684a
Madaraka02 <victormadaraka02@gmail.com>
2022-02-08 12:15:01 -0800

    added users

--- bank/models.py
+++ bank/models.py
@@ -1,8 +1,5 @@
-from statistics import mode
-from django.db import models
-from django.contrib.auth.models import User
-from django.forms import DateTimeField
 
+from django.db import models
 # Create your models here.
 
 class Bank(models.Model):
@@ -13,7 +10,7 @@ class Bank(models.Model):
         return self.name
 
 class Accounts(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
     acc_balance = models.DecimalField(max_digits=15, decimal_places=2)
     date_created = models.DateTimeField(auto_now_add = True)
