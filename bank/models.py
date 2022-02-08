24e8b3e8f3f5f5971eda3e99451075d62532e77d
Madaraka02 <victormadaraka02@gmail.com>
2022-02-08 12:23:11 -0800

    added bank models

--- bank/models.py
+++ bank/models.py
@@ -10,7 +10,7 @@ class Bank(models.Model):
         return self.name
 
 class Accounts(models.Model):
-    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    owner = models.ForeignKey('auth.User', related_name='account', on_delete=models.CASCADE)
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
     acc_balance = models.DecimalField(max_digits=15, decimal_places=2)
     date_created = models.DateTimeField(auto_now_add = True)
--- bank/serializers.py
+++ bank/serializers.py
@@ -1,7 +1,18 @@
+from pyexpat import model
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
+    account = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'account']
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')       
+
+    class Meta:
+        model = Accounts
+        fields = ['id', 'owner', 'bank', 'acc_balance', 'date_created'] 
